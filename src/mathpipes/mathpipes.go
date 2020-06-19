b52fdb0bface91fbd6179745d90802765a993de7
Rafael Algara <ralgara@gmail.com>
2020-06-19 12:57:13 -0400

    Better math pipeline example

--- src/mathpipes/mathpipes.go
+++ src/mathpipes/mathpipes.go
@@ -4,58 +4,60 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"sync"
 )
 
+// Work item struct: two operands and a function object
 type Op struct {
-	x float64
-	y float64
-	f func(float64, float64) float64
+	x  float64
+	y  float64
+	fn Function
 }
 
+// Function object: name and func
+type Function struct {
+	name string
+	impl func(float64, float64) float64
+}
+
+// Pipeline channel
 var c chan Op = make(chan Op)
 
-func fmult(x, y float64) float64 {
-	return x * y
-}
-
-func fsum(x, y float64) float64 {
-	return x + y
-}
-
-func send(op Op) {
-	c <- op
-}
-
-func execute() {
-	op := <-c
-	res := op.f(op.x, op.y)
-	fmt.Printf("%v = %v\n", op, res)
-	results <- true
-}
-
 var results chan bool = make(chan bool)
+
+// WaitGroup to ensure main
+var wg sync.WaitGroup
 
 func main() {
 	fmt.Println("Start")
 	n := 10
-	funcs := []func(float64, float64) float64{
-		fsum,
-		fmult,
-		math.Max,
+	// Available functions, some from standard library, some custom
+	funcs := []Function{
+		Function{"min", math.Min},
+		Function{"max", math.Max},
+		Function{"foo", func(x, y float64) float64 {
+			return x*x + y*y
+		}},
 	}
 
+	// Send random Ops to channel
 	for i := 0; i < n; i++ {
-		go send(Op{
-			x: rand.Float64() * 10,
-			y: rand.Float64() * 10,
-			f: funcs[rand.Intn(len(funcs))],
+		// Inline goroutine to write Op to channel without blocking
+		// Op function and operands are random
+		go func(op Op) {
+			c <- op
+		}(Op{
+			x:  rand.Float64() * 10,
+			y:  rand.Float64() * 10,
+			fn: funcs[rand.Intn(len(funcs))],
 		})
+		//time.Sleep(time.Duration(1) * time.Second)
 	}
+	// Execute all Ops from the channel
 	for i := 0; i < n; i++ {
-		go execute()
-	}
-	for i := 0; i < n; i++ {
-		<-results
+		op := <-c
+		res := op.fn.impl(op.x, op.y)
+		fmt.Printf("%s(%0.2f,%0.2f)= %0.2f\n", op.fn.name, op.x, op.y, res)
 	}
 	fmt.Println("End")
 }
